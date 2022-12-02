2fe14763ca0f3544f98e67f2c4a72b646a50fa13
Tomasz Szymaniec <tomek.szymaniec@gmail.com>
2022-12-02 13:51:24 +0000

    Q2 solution

--- 2022/q1.py
+++ 2022/q1.py
@@ -4,17 +4,18 @@ from main import read_input
 
 def calculate_group_sums(raw_input: List[str]) -> List[int]:
     # Construct a single string with all the elements then split on
-    # each grouping of items (separated by a double underscore)
+    # each grouping of items (separated by a double underscore).
     grouped = '_'.join(raw_input).split("__")
     # For each grouping, extract the integer values and sum together.
     return [sum(list(map(lambda x: int(x), elem.split('_')))) for elem in grouped]
 
 
-def part1():
+def part1() -> int:
     return max(calculate_group_sums(read_input("q1.txt")))
 
 
-def part2():
+def part2() -> int:
     group_sums = calculate_group_sums(read_input("q1.txt"))
     group_sums.sort(reverse=True)
+    # Sum the top three values.
     return sum(group_sums[0:3])
