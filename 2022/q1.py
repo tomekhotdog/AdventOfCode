788209f0cf8d9f801c8ee6913dc48b0db1f79158
Tomasz Szymaniec <tomek.szymaniec@gmail.com>
2022-12-02 11:45:53 +0000

    Q1: Refactored for a more functional approach

--- 2022/q1.py
+++ 2022/q1.py
@@ -1,31 +1,20 @@
-import typing
+from typing import List
 from main import read_input
 
 
+def calculate_group_sums(raw_input: List[str]) -> List[int]:
+    # Construct a single string with all the elements then split on
+    # each grouping of items (separated by a double underscore)
+    grouped = '_'.join(raw_input).split("__")
+    # For each grouping, extract the integer values and sum together.
+    return [sum(list(map(lambda x: int(x), elem.split('_')))) for elem in grouped]
+
+
 def part1():
-    lines = read_input("q1_example.txt")
-    current_total_max = 0
-    current_total = 0
-    for line in lines:
-        if line == "":
-            current_total_max = current_total_max if current_total_max > current_total else current_total
-            current_total = 0
-        else:
-            current_total += int(line)
-    return current_total_max
+    return max(calculate_group_sums(read_input("q1.txt")))
 
 
 def part2():
-    lines = read_input("q1.txt")
-    lines.append("")
-    max_items = [0, 0, 0]
-    current_total = 0
-    for line in lines:
-        if line == "":
-            if current_total > min(max_items):
-                max_items.remove(min(max_items))
-                max_items.append(current_total)
-            current_total = 0
-        else:
-            current_total += int(line)
-    return sum(max_items)
+    group_sums = calculate_group_sums(read_input("q1.txt"))
+    group_sums.sort(reverse=True)
+    return sum(group_sums[0:3])
